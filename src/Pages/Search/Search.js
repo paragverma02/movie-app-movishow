963c14d7a8d99a1b9c3e91d51a2970b3716333fc
paragverma02 <parag_ihm@yahoo.com>
2024-10-18 21:19:31 +0530

    updated code

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import {
+  Button,
+  createTheme,
+  Tab,
+  Tabs,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
+import "./Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Search = () => {
+  const [type, setType] = useState(0);
+  const [searchText, setSearchText] = useState("");
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
+  const fetchSearch = async () => {
+    if (!searchText) return; // Prevent the API call if searchText is empty
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+          process.env.REACT_APP_API_KEY
+        }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+      );
+      setContent(data.results);
+      setNumOfPages(data.total_pages);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    if (searchText) {
+      fetchSearch();
+    }
+    // eslint-disable-next-line
+  }, [type, page]);
+
+  const handleSearchEnter = (event) => {
+    if (event.key === "Enter") {
+      fetchSearch();
+    }
+  };
+
   return (
     <div>
-      <span className='pageTitle'>Search</span>
+      <ThemeProvider theme={darkTheme}>
+        <div className="search">
+          <TextField
+            style={{ flex: 1 }}
+            className="searchBox"
+            label="Search"
+            variant="filled"
+            onChange={(e) => setSearchText(e.target.value)}
+            onKeyPress={handleSearchEnter} // Trigger search on Enter key press
+          />
+          <Button
+            onClick={fetchSearch}
+            variant="contained"
+            style={{ marginLeft: 10 }}
+          >
+            <SearchIcon fontSize="large" />
+          </Button>
+        </div>
+        <Tabs
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+          style={{ paddingBottom: 5 }}
+          aria-label="disabled tabs example"
+        >
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+        </Tabs>
+      </ThemeProvider>
+      <div className="trending">
+        {content.length > 0 &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText && content.length === 0 && (
+          <h2>{type ? "No Series Found" : "No Movies Found"}</h2>
+        )}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Search;
